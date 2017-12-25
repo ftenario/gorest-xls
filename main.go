39f44ff2bd3cba32934a62fa38eb2f0b75c017ad
Ferdinand Enario <ftenario@yahoo.com>
2017-12-24 16:04:57 -0800

    add .gitignore. Add dummy data to excel file

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
   "gorest-xls/random"
   "os"
   "io/ioutil"
+  "strconv"
 )
 
 func main() {
@@ -24,7 +25,38 @@ func ExcelHandler(w http.ResponseWriter, r *http.Request) {
   xlsx := excelize.NewFile()
 
   xlsx.SetCellValue("Sheet1", "A1", "Test Data")
+  xlsx.SetCellValue("Sheet1", "A2", "X-Axis")
+  xlsx.SetCellValue("Sheet1", "B2", "Y-Axis")
+  xlsx.SetCellValue("Sheet1", "C2", "Z-Axis")
 
+  //create a dummy data
+  data := [][]uint32{
+    {10, 2, 12},
+    {20, 4, 55},
+    {30, 10, 15},
+    {40, 11, 11},
+    {50, 12, 17},
+    {60, 20, 17},
+    {70, 35, 15},
+    {80, 10, 45},
+    {90, 86, 55},
+    {100, 44, 60},
+    {110, 55, 90},
+    {120, 70, 100},
+  }
+
+  //start row ehere we put the data
+  row := 3
+
+  //loop over the data
+  for i := range data {
+    xlsx.SetCellValue("Sheet1", "A" + strconv.Itoa(row), data[i][0])
+    xlsx.SetCellValue("Sheet1", "B" + strconv.Itoa(row), data[i][1])
+    xlsx.SetCellValue("Sheet1", "C" + strconv.Itoa(row), data[i][2])
+    row++
+  }
+
+  //generate a temp filename
   var name = "./" + random.Generate(10)
   err := xlsx.SaveAs(name)
 
